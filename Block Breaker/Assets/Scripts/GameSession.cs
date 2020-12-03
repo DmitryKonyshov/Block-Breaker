f078c36d6860cc6581c00c610c3d9341b07e530c
Dmitry <dmitrykonyshov@gmail.com>
2020-12-03 21:24:05 +0200

    Autoplay for Playtesting

--- Block Breaker/Assets/Scripts/GameSession.cs
+++ Block Breaker/Assets/Scripts/GameSession.cs
@@ -10,6 +10,7 @@ public class GameSession : MonoBehaviour
     [Range(0.1f, 10.0f)][SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoPlayEnabled;
 
     //State variables
     [SerializeField] int currentScore = 0;
@@ -48,5 +49,10 @@ public class GameSession : MonoBehaviour
     public void ResetGame()
     {
         Destroy(gameObject);
+    }
+
+    public bool IsAutoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
     }
 }
--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -8,18 +8,35 @@ public class Paddle : MonoBehaviour
     [SerializeField] float minX = 1f;
     [SerializeField] float maxX = 15f;
     [SerializeField] float screenWidthInUnits = 16f;
+    
+    // Cached references
+    private GameSession theGameSession;
+    private Ball theBall;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        theGameSession = FindObjectOfType<GameSession>();
+        theBall = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
-        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
         transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if (FindObjectOfType<GameSession>().IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits
+        }
     }
 }
